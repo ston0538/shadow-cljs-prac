a4861ee2038def0319c37d682eab6f7edae99d22
KHK <ston0538@gmail.com>
2020-09-02 04:09:01 +0900

    [UPDATE] setting, helix react
    [notion](https://www.notion.so/khk0613/shadow-cljs-d2d9dc1a65d847c4b4d513e750529b54)

---
+++ src/cljs/app/core.cljs
@@ -0,0 +1,22 @@
+(ns app.core
+  (:require [helix.core :refer [defnc $]]
+            [helix.hooks :as hooks]
+            [helix.dom :as d]
+            ["react-dom" :as rdom]))
+(defnc greeting
+  "A component which greets a user."
+  [{:keys [name]}]
+  ;; use helix.dom to create DOM elements
+  (d/div "Hello, " (d/strong name) "!"))
+(defnc app []
+  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
+    (d/div
+     (d/h1 "Welcome!")
+      ;; create elements out of components
+     ($ greeting {:name (:name state)})
+     (d/input {:value (:name state)
+               :on-change #(set-state assoc :name (.. % -target -value))}))))
+(defn render []
+  (rdom/render ($ app) (js/document.getElementById "root")))
+(defn ^:export init []
+  (render))
